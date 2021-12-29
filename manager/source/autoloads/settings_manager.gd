6bd8838c727bc2f7b40f06dc2545edc0818ebbdd
Aiden Storey <git@storey.dev>
2021-12-29 01:05:42 -0500

    Comment out settings manager temporarily

--- manager/source/autoloads/settings_manager.gd
+++ manager/source/autoloads/settings_manager.gd
@@ -66,21 +66,22 @@ func get_setting(name, default = null):
 	var path: Array = name.split(".")
 	var location: Array = __settings
 
-	for index in range(path.size() - 1):
-		var part: String = path[index]
-
-		if location.has(part):
-			location = location.get(part)
-		else:
-			Logger.warn("Could not get setting '%s'" % name)
-			return default
-
-	var setting_name: String = path[path.size() - 1]
-	if location.has(setting_name):
-		return location.get(setting_name)
-	else:
-		Logger.warn("Could not get setting '%s'" % name)
-		return default
+#	for index in range(path.size() - 1):
+#		var part: String = path[index]
+#
+#		if location.has(part):
+#			location = location.get(part)
+#		else:
+#			Logger.warn("Could not get setting '%s'" % name)
+#			return default
+#
+#	var setting_name: String = path[path.size() - 1]
+#	if location.has(setting_name):
+#		return location.get(setting_name)
+#	else:
+#		Logger.warn("Could not get setting '%s'" % name)
+#		return default
+	return default
 
 
 func save_settings() -> void:
@@ -96,20 +97,20 @@ func set_setting(name: String, value, save: bool = false) -> void:
 # Private methods
 func __setting_changed(name: String, value) -> void:
 	var path: Array = name.split("/")
-	var location: Dictionary = __settings
+	var location: Array = __settings
 
-	for index in range(path.size() - 1):
-		var part: String = path[index]
-
-		if location.has(part):
-			location = location.get(part)
-		else:
-			Logger.warn("Could not update setting '%s'" % name)
-			return
-
-	var setting_name: String = path[path.size() - 1]
-	if location.has(setting_name):
-		location[setting_name] = value
-	else:
-		Logger.warn("Could not update setting '%s'" % name)
-		return
+#	for index in range(path.size() - 1):
+#		var part: String = path[index]
+#
+#		if location.has(part):
+#			location = location.get(part)
+#		else:
+#			Logger.warn("Could not update setting '%s'" % name)
+#			return
+#
+#	var setting_name: String = path[path.size() - 1]
+#	if location.has(setting_name):
+#		location[setting_name] = value
+#	else:
+#		Logger.warn("Could not update setting '%s'" % name)
+#		return
