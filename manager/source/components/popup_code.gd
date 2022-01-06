89c2fb90d1ee3d84d7f1d9237d6d9e51aa2025aa
Aiden Storey <git@storey.dev>
2022-01-05 23:18:52 -0500

    Add error checking to code runner

--- manager/source/components/popup_code.gd
+++ manager/source/components/popup_code.gd
@@ -1,13 +1,46 @@
 extends PopupDialog
 
+# Private constants
 
-onready var input_code: TextEdit = $input_code
+const __BASE_SOURCE_CODE = "func handle(state: Dictionary) -> void:\n\t%s"
+
+# Private variables
+
+onready var __input_code: TextEdit = $input_code
+
+var __timer_debounce: Timer = null
+
+
+# Lifecycle methods
 
 func _ready():
+	__timer_debounce = Timer.new()
+	__timer_debounce.one_shot = true
+	__timer_debounce.connect("timeout", self, "__check_code")
+	add_child(__timer_debounce)
+
+	__input_code.connect("text_changed", self, "__input_code_text_changed")
 	connect("popup_hide", self, "__run")
 
 
-const BASE_SOURCE_CODE = "func handle(state: Dictionary) -> void:\n\t%s"
+# Private methods
+
+func __check_code() -> void:
+	var raw_code: String = __input_code.text.replace("\n", "\n\t")
+	var code: String = __BASE_SOURCE_CODE % raw_code
+
+	var script: GDScript = GDScript.new()
+	script.set_source_code(code)
+
+	var thread: Thread = Thread.new()
+	thread.start(self, "__reload_script", script)
+
+	var error: int = thread.wait_to_finish()
+	print(error) # TODO: Show error to user
+
+
+func __input_code_text_changed() -> void:
+	self.__timer_debounce.start(0.5)
 
 
 func __reload_script(script: GDScript) -> int:
@@ -15,8 +48,8 @@ func __reload_script(script: GDScript) -> int:
 
 
 func __run() -> void:
-	var raw_code: String = input_code.text.replace("\n", "\n\t")
-	var code: String = BASE_SOURCE_CODE % raw_code
+	var raw_code: String = __input_code.text.replace("\n", "\n\t")
+	var code: String = __BASE_SOURCE_CODE % raw_code
 
 	var script: GDScript = GDScript.new()
 	script.set_source_code(code)
