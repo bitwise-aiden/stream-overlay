e786ee05064d9b514bfbe93edb4a217ea4cbd957
Aiden Storey <git@storey.dev>
2022-01-29 03:28:28 -0500

    Add initial profile manipulation

--- manager/source/main.gd
+++ manager/source/main.gd
@@ -1,23 +1,114 @@
 extends VBoxContainer
 
 
+# Private constants
+
+const __PATH_SETTING: String = "setting"
+
+
+
 # Private variables
 
+onready var __dialog_file_path: FileDialog = $tabs_content/dialog_file_path
+onready var __dialog_profile_name: ConfirmationDialog = $tabs_content/dialog_profile_name
+onready var __menu_file: PopupMenu = $container_menu/file.get_popup()
+onready var __menu_profile: PopupMenu = $container_menu/profile.get_popup()
 onready var __tab_command: TabCommand = $tabs_content/Command
 
-var __data: DataApplication = DataApplication.new()
+var __data_application: DataApplication
+var __data_profile: DataProfile
+var __profile_names: Array = []
 
 
 # Lifecycle methods
 
 func _ready() -> void:
+	__data_application = __load_application()
+	__data_profile = ProfileManager.load_profile(__data_application.profile)
+	__profile_names = FileManager.files_in_directory("profiles")
 
-	if FileManager.file_exists("test_settings"):
-		var result = FileManager.load_file("test_settings")
-		__data = str2var(result)
+	if __data_application.profile != __data_profile.name:
+		# TODO: Show error as profile is missing
 
-	__tab_command.set_data(__data.command)
+		__data_application.profile = __data_profile.name
+		__save_application()
+
+	__tab_command.set_data(__data_profile.tab_command)
+
+	# TODO: Make this less hacky
+	Event.disconnect("data_changed", ProfileManager, "save_profile")
+	Event.connect("data_changed", ProfileManager, "save_profile", [__data_profile])
+
+	__menu_profile.connect("id_pressed", self, "__button_profile_pressed")
 
 
 func _exit_tree() -> void:
-	FileManager.save_file("test_settings", var2str(__data))
+	__save_application()
+	ProfileManager.save_profile(__data_profile)
+
+
+# Private methods
+
+func __button_profile_pressed(id: int) -> void:
+	var load_profile: DataProfile = null
+
+	ProfileManager.save_profile(__data_profile)
+
+	match id:
+		0, 1, 2:
+			var profile_name: String = yield(__dialog_profile_name.get_profile_name(), "completed")
+
+			# TODO: Handle existing file names
+			if !profile_name:
+				return
+
+			match id:
+				0:
+					load_profile = ProfileManager.create_profile(profile_name)
+				1:
+					load_profile = ProfileManager.duplicate_profile(profile_name, __data_profile)
+				2:
+					ProfileManager.rename_profile(profile_name, __data_profile)
+					load_profile = __data_profile
+		3:
+			ProfileManager.delete_profile(__data_profile)
+			load_profile = ProfileManager.load_profile("default")
+		4, 5:
+			var file_path: String = yield(__dialog_file_path.get_file_path(id == 4), "completed")
+
+			# TODO: Handle if file isn't json
+			if !file_path:
+				return
+
+			match id:
+				4:
+					var json: Dictionary = FileManager.load_json(file_path)
+					Data.from_json(json)
+				5:
+					var json: Dictionary = __data_profile.json()
+					FileManager.save_json(file_path, json)
+		6:
+			pass # ignore
+		7:
+			pass
+
+	if !load_profile:
+		return
+
+	__data_profile = load_profile
+	__data_application.profile = load_profile.name
+
+	get_tree().reload_current_scene()
+
+
+func __load_application() -> DataApplication:
+	if FileManager.file_exists(__PATH_SETTING):
+		var result = FileManager.load_file(__PATH_SETTING)
+		return str2var(result)
+
+	return DataApplication.new()
+
+
+func __save_application() -> void:
+	FileManager.save_file(__PATH_SETTING, var2str(__data_application))
+
